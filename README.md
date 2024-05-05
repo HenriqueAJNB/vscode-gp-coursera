bd4cd28663f571350fa198a5b418fbedeb2ff33a
Henrique Branco <henrique.ajnbranco@gmail.com>
2024-05-05 17:33:19 -0300

    feat: upload code example

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# vscode-gp-coursera
-Codebase for VSCode for Developers: Set up a professional environment Guided Project on Coursera
+# Advanced VSCode for Software Developers
+
+Codebase for Guided Project "VSCode for Developers: Set up a professional environment" on Coursera platform
+
+## Basic project setup
+
+- Install virtual enviroment with its dependencies: `poetry install`
+- Activate it: `poetry shell`
+- To run the API, use the command: `uvicorn src.app:app --reload`
---
+++ src/app.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, HTTPException
+
+from src.database import get_db, startup_event
+from src.models import Item
+
+app = FastAPI()
+app.add_event_handler("startup", startup_event)
+
+
+@app.post("/items/")
+def create_item(item: Item):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO items (name, price, is_offer) VALUES (?, ?, ?)",
+        (item.name, item.price, int(item.is_offer) if item.is_offer else None),
+    )
+    conn.commit()
+    item.id = cursor.lastrowid
+    return item
+
+
+@app.get("/items/")
+def read_items():
+    conn = get_db()
+    items = conn.execute("SELECT * FROM items").fetchall()
+    return [dict(item) for item in items]
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    conn = get_db()
+    item = conn.execute("SELECT * FROM items WHERE id = ?", (item_id,)).fetchone()
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return dict(item)
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    conn = get_db()
+    conn.execute(
+        "UPDATE items SET name = ?, price = ?, is_offer = ? WHERE id = ?",
+        (item.name, item.price, int(item.is_offer) if item.is_offer else None, item_id),
+    )
+    conn.commit()
+    return item
+
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    conn = get_db()
+    conn.execute("DELETE FROM items WHERE id = ?", (item_id,))
+    conn.commit()
+    return {"message": "Item deleted"}

12a2996132aa2767a06284c1f0f5e2b8243603a6
Henrique Branco <henrique.ajnbranco@gmail.com>
2024-06-21 21:54:58 -0300

    transiction task02 to task03

--- src/database.py
+++ src/database.py
@@ -1,10 +1,15 @@
 import sqlite3
+
+
 def get_db():
     conn = sqlite3.connect("items.db")
     conn.row_factory = sqlite3.Row
     return conn
+
+
 def startup_event():
     conn = get_db()
     conn.execute(
-        "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name TEXT, price REAL, is_offer INTEGER)")
+        "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name TEXT, price REAL, is_offer INTEGER)"
+    )
     conn.commit()
--- src/models.py
+++ src/models.py
@@ -1,5 +1,8 @@
 from typing import Optional
+
 from pydantic import BaseModel
+
+
 class Item(BaseModel):
     id: Optional[int] = None
     name: str

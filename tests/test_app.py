61856ce1d408718c93f62f1aa8400f314ca44c12
Henrique Branco <henrique.ajnbranco@gmail.com>
2024-07-05 21:30:11 -0300

    fixed applied on tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ import unittest
 from fastapi.testclient import TestClient
 
 from src.app import app
-from src.models import Item
+from src.models import StockKeepingUnit
 
 client = TestClient(app)
 
@@ -11,17 +11,20 @@ client = TestClient(app)
 class TestStringMethods(unittest.TestCase):
     def test_create_items(self):
         items = [
-            Item(id=1, name="Laptop", price=999.99, is_offer=True),
-            Item(id=2, name="Smartphone", price=599.99, is_offer=False),
-            Item(id=3, name="Headphones", price=199.99, is_offer=True),
-            Item(id=4, name="Monitor", price=299.99, is_offer=False),
-            Item(id=5, name="Keyboard", price=49.99, is_offer=True),
-            Item(id=6, name="Mouse", price=29.99, is_offer=False),
-            Item(id=7, name="Printer", price=149.99, is_offer=True),
-            Item(id=8, name="Webcam", price=89.99, is_offer=False),
-            Item(id=9, name="External Hard Drive", price=79.99, is_offer=True),
-            Item(id=10, name="USB Flash Drive", price=19.99, is_offer=False),
-            # Item(id=1, name="Laptop", price=999.99, is_offer=True)
+            StockKeepingUnit(id=1, name="Laptop", price=999.99, is_offer=True),
+            StockKeepingUnit(id=2, name="Smartphone", price=599.99, is_offer=False),
+            StockKeepingUnit(id=3, name="Headphones", price=199.99, is_offer=True),
+            StockKeepingUnit(id=4, name="Monitor", price=299.99, is_offer=False),
+            StockKeepingUnit(id=5, name="Keyboard", price=49.99, is_offer=True),
+            StockKeepingUnit(id=6, name="Mouse", price=29.99, is_offer=False),
+            StockKeepingUnit(id=7, name="Printer", price=149.99, is_offer=True),
+            StockKeepingUnit(id=8, name="Webcam", price=89.99, is_offer=False),
+            StockKeepingUnit(
+                id=9, name="External Hard Drive", price=79.99, is_offer=True
+            ),
+            StockKeepingUnit(
+                id=10, name="USB Flash Drive", price=19.99, is_offer=False
+            ),
         ]
 
         for item in items:
